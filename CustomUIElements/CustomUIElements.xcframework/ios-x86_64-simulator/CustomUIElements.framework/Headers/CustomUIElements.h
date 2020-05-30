30c4b7ee2e5b4fd2ff56e072a3085ef626d0031e
Vinicius Santos Ricci <vinicius.ricci4@gmail.com>
2020-05-30 00:33:30 -0300

    First library

---
+++ CustomUIElements/CustomUIElements.xcframework/ios-x86_64-simulator/CustomUIElements.framework/Headers/CustomUIElements.h
@@ -0,0 +1,19 @@
+//
+//  CustomUIElements.h
+//  CustomUIElements
+//
+//  Created by Vinicius Santos Ricci on 29/05/20.
+//  Copyright © 2020 Vinicius Santos Ricci. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomUIElements.
+FOUNDATION_EXPORT double CustomUIElementsVersionNumber;
+
+//! Project version string for CustomUIElements.
+FOUNDATION_EXPORT const unsigned char CustomUIElementsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomUIElements/PublicHeader.h>
+
+
