c8b78de57ab6544a612c11659a6520279d315b4e
Vinicius Santos Ricci <vinicius.ricci4@gmail.com>
2020-06-05 06:27:01 -0300

    new version

--- CustomUIElements/0.0.1/CustomUIElements.xcframework/ios-arm64/CustomUIElements.framework/Headers/CustomUIElements-Swift.h
+++ CustomUIElements/0.0.1/CustomUIElements.xcframework/ios-arm64/CustomUIElements.framework/Headers/CustomUIElements-Swift.h
@@ -181,6 +181,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import ObjectiveC;
 @import UIKit;
 #endif
 
@@ -198,6 +199,24 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="CustomUIElements",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
+
+
+SWIFT_CLASS("_TtC16CustomUIElements15MainCoordinator")
+@interface MainCoordinator : NSObject
+- (nonnull instancetype)init SWIFT_UNAVAILABLE;
++ (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
+@end
+
+@class NSBundle;
+@class NSCoder;
+
+SWIFT_CLASS("_TtC16CustomUIElements18TESTViewController")
+@interface TESTViewController : UIViewController
+- (void)viewDidLoad;
+- (void)viewWillAppear:(BOOL)animated;
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
+@end
 
 
 
